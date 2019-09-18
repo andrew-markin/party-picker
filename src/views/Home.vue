de9ba9ca0258d5c52eaa6fc350e6dd6900a165a0
Andrew Markin <andrew.n.markin@gmail.com>
2019-09-19 02:26:28 +0300

    Basic backend interaction implemented for testing purposes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
-    <h1>This is the Party Picker!</h1>
+    <h1>{{message}}</h1>
   </div>
 </template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    updateMessage () {
+      axios.get('/').then(res => {
+        this.message = res.data.success ? res.data.message : 'Invalid backend response :('
+      }).catch(err => {
+        console.log('Backend request error:', err)
+        this.message = 'Backend request failed :('
+      })
+    }
+  },
+  mounted () {
+    this.$nextTick(this.updateMessage)
+  }
+}
+
+</script>
